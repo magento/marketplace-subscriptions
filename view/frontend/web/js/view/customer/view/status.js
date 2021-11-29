305691532023fa021a22b56e6706982318e84212
Oleksandr Baranovskyi <obaranovskyi@magento.com>
2021-11-29 09:24:39 -0600

    - fix text when status get canceled

--- view/frontend/web/js/view/customer/view/status.js
+++ view/frontend/web/js/view/customer/view/status.js
@@ -43,7 +43,7 @@ define([
                 updatedStatus = event.target.value;
 
             if (event.target.value == 3) {
-                if (!confirm('Are you sure you want to cancel your subscription?. In this case, you will not be able to activate it or get your money back.')) {
+                if (!confirm('Are you sure you want to cancel your subscription?')) {
                     $('#select-status option[value=1]').attr('selected','selected');
                     return;
                 } else {
